eedb27ecb7379e1ea4876689fc52ebabbdd82ccf
solomiiabbv <solomiya.babijiv@kpk-lp.com.ua>
2026-03-19 14:06:18 +0200

    Stage 1: азовий екран + кольори

--- app/src/main/java/com/example/jewelrystore/MainActivity.kt
+++ app/src/main/java/com/example/jewelrystore/MainActivity.kt
@@ -1,20 +1,35 @@
 package com.example.jewelrystore
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val tvGreeting = findViewById<TextView>(R.id.tvGreeting)
+        val btnManageProducts = findViewById<Button>(R.id.btnManageProducts)
+        val btnInfoList = findViewById<Button>(R.id.btnInfoList)
+        val btnProfile = findViewById<Button>(R.id.btnProfile)
+        val btnLogout = findViewById<Button>(R.id.btnLogout)
+
+        // Кнопки зараз просто показують Toast
+        btnManageProducts.setOnClickListener {
+            Toast.makeText(this, "Керування товарами", Toast.LENGTH_SHORT).show()
+        }
+        btnInfoList.setOnClickListener {
+            Toast.makeText(this, "Інформаційний список", Toast.LENGTH_SHORT).show()
+        }
+        btnProfile.setOnClickListener {
+            Toast.makeText(this, "Профіль адміністратора", Toast.LENGTH_SHORT).show()
+        }
+        btnLogout.setOnClickListener {
+            Toast.makeText(this, "Вихід", Toast.LENGTH_SHORT).show()
         }
     }
 }
