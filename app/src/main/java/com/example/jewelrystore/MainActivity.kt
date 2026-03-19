bc6298e8a6e6e80912351707fa75ef5f9b324f63
solomiiabbv <solomiya.babijiv@kpk-lp.com.ua>
2026-03-19 13:53:38 +0200

    базове меню

--- app/src/main/java/com/example/jewelrystore/MainActivity.kt
+++ app/src/main/java/com/example/jewelrystore/MainActivity.kt
@@ -1,20 +1,48 @@
 package com.example.jewelrystore
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var tvWelcome: TextView
+    private lateinit var btnManageProducts: Button
+    private lateinit var btnInfoList: Button
+    private lateinit var btnProfile: Button
+    private lateinit var btnLogout: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // знаходимо елементи через findViewById
+        tvWelcome = findViewById(R.id.tvWelcome)
+        btnManageProducts = findViewById(R.id.btnManageProducts)
+        btnInfoList = findViewById(R.id.btnInfoList)
+        btnProfile = findViewById(R.id.btnProfile)
+        btnLogout = findViewById(R.id.btnLogout)
+
+        // Текст привітання
+        tvWelcome.text = "Ласкаво просимо в ювелірний магазин!"
+
+        // обробка кнопок
+        btnManageProducts.setOnClickListener {
+            Toast.makeText(this, "Відкрито керування товарами", Toast.LENGTH_SHORT).show()
+        }
+
+        btnInfoList.setOnClickListener {
+            Toast.makeText(this, "Відкрито інформаційний список", Toast.LENGTH_SHORT).show()
+        }
+
+        btnProfile.setOnClickListener {
+            Toast.makeText(this, "Відкрито профіль адміністратора", Toast.LENGTH_SHORT).show()
+        }
+
+        btnLogout.setOnClickListener {
+            Toast.makeText(this, "Вихід з системи", Toast.LENGTH_SHORT).show()
         }
     }
 }
