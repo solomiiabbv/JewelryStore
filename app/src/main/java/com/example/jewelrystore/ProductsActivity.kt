008ffab3e6f84c7b52e38c4965c49e697b8f874c
solomiiabbv <solomiya.babijiv@kpk-lp.com.ua>
2026-03-25 00:55:33 +0200

    + 5 товарів

--- app/src/main/java/com/example/jewelrystore/ProductsActivity.kt
+++ app/src/main/java/com/example/jewelrystore/ProductsActivity.kt
@@ -16,18 +16,17 @@ class ProductsActivity : AppCompatActivity() {
         val rvProducts = findViewById<RecyclerView>(R.id.rvProducts)
         val btnAdd = findViewById<Button>(R.id.btnAdd)
 
-        // Приклад товарів із локальними картинками
         val productList = listOf(
-            Product("Золоте кільце", "1200 грн", R.drawable.gold_ring),
-            Product("Срібний браслет", "800 грн", R.drawable.silver_bracelet),
-            Product("Сережки з перлами", "950 грн", R.drawable.pearl_earrings),
-            Product("Кулон з діамантом", "5000 грн", R.drawable.diamond_pendant),
-            Product("Брошка 'Роза'", "450 грн", R.drawable.rose_brooch),
-            Product("Чоловічий годинник", "2500 грн", R.drawable.mens_watch),
-            Product("Жіночий годинник", "2200 грн", R.drawable.womens_watch),
-            Product("Кольє з каменем", "1800 грн", R.drawable.stone_necklace),
-            Product("Срібна каблучка", "700 грн", R.drawable.silver_ring),
-            Product("Браслет з намистин", "650 грн", R.drawable.beaded_bracelet)
+            Product("Золоте кільце", "1200 грн", R.drawable.gold_ring, "Кільця"),
+            Product("Срібний браслет", "800 грн", R.drawable.silver_bracelet, "Браслети"),
+            Product("Сережки з перлами", "950 грн", R.drawable.pearl_earrings, "Сережки"),
+            Product("Кулон з діамантом", "5000 грн", R.drawable.diamond_pendant, "Кулони"),
+            Product("Брошка 'Роза'", "450 грн", R.drawable.rose_brooch, "Брошки"),
+            Product("Чоловічий годинник", "2500 грн", R.drawable.mens_watch, "Годинники"),
+            Product("Жіночий годинник", "2200 грн", R.drawable.womens_watch, "Годинники"),
+            Product("Кольє з каменем", "1800 грн", R.drawable.stone_necklace, "Кольє"),
+            Product("Срібна каблучка", "700 грн", R.drawable.silver_ring, "Кільця"),
+            Product("Браслет з намистин", "650 грн", R.drawable.beaded_bracelet, "Браслети")
         )
 
         rvProducts.layoutManager = LinearLayoutManager(this)
