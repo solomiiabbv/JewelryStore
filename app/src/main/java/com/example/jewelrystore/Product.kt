3069d925d6fd434b093a963e98fbae23fcbbfd11
solomiiabbv <solomiya.babijiv@kpk-lp.com.ua>
2026-03-25 00:53:48 +0200

    категоія

--- app/src/main/java/com/example/jewelrystore/Product.kt
+++ app/src/main/java/com/example/jewelrystore/Product.kt
@@ -3,5 +3,6 @@ package com.example.jewelrystore
 data class Product(
     val name: String,
     val price: String,
-    val imageResId: Int
+    val imageResId: Int,
+    val catecory: String
 )
--- app/src/main/java/com/example/jewelrystore/ProductsActivity.kt
+++ app/src/main/java/com/example/jewelrystore/ProductsActivity.kt
@@ -22,7 +22,12 @@ class ProductsActivity : AppCompatActivity() {
             Product("Срібний браслет", "800 грн", R.drawable.silver_bracelet),
             Product("Сережки з перлами", "950 грн", R.drawable.pearl_earrings),
             Product("Кулон з діамантом", "5000 грн", R.drawable.diamond_pendant),
-            Product("Брошка 'Роза'", "450 грн", R.drawable.rose_brooch)
+            Product("Брошка 'Роза'", "450 грн", R.drawable.rose_brooch),
+            Product("Чоловічий годинник", "2500 грн", R.drawable.mens_watch),
+            Product("Жіночий годинник", "2200 грн", R.drawable.womens_watch),
+            Product("Кольє з каменем", "1800 грн", R.drawable.stone_necklace),
+            Product("Срібна каблучка", "700 грн", R.drawable.silver_ring),
+            Product("Браслет з намистин", "650 грн", R.drawable.beaded_bracelet)
         )
 
         rvProducts.layoutManager = LinearLayoutManager(this)
